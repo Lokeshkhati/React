6c28c13cc7722dc02a1d650f1c31801d04bada9c
LOKESH <lokeshkhati005@gmail.com>
2021-12-25 21:10:48 +0530

    social links added

--- business-card/src/components/Footer.js
+++ business-card/src/components/Footer.js
@@ -7,16 +7,16 @@ import github from "../images/github.svg";
 export default function Footer() {
   return (
     <footer>
-      <a href="" target="_blank">
+      <a href="https://twitter.com/Lokeshkhati12" target="_blank">
         <img className="footer-link" src={twitter} />
       </a>
-      <a href="" target="_blank">
+      <a href="#" target="_blank">
         <img className="footer-link" src={instagram} />
       </a>
-      <a href="" target="_blank">
+      <a href="https://hashnode.com/@Lokesh005" target="_blank">
         <img className="footer-link" src={hashnode} />
       </a>
-      <a href="" target="_blank">
+      <a href="https://github.com/Lokeshkhati" target="_blank">
         <img className="footer-link" src={github} />
       </a>
     </footer>
