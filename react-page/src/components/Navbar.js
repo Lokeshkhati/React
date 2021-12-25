73099c845fa76b9abc991f820c23cc74e38004a3
LOKESH <lokeshkhati005@gmail.com>
2021-12-25 13:59:55 +0530

    completed

--- react-page/src/components/Navbar.js
+++ react-page/src/components/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react"
+import images from "../images/react-logo.png"
 
 export default function Navbar(){
     return(
-        <h1>Navbar Component</h1>
+        <nav className="nav">
+            <img className="nav-logo" src={images}/>
+            <h3 className="nav-logo_text">React Facts </h3>
+            <h4 className="nav-title">React Course - Project 1</h4>
+        </nav>
     )
 }
